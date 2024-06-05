9d1c1977407ef061f97df7d9546ce918f6a974a7
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-06-05 09:04:37 +0000

    Fixes the last bug

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
+"""
+This module provides a function to query the Reddit API
+and return the number of subscribers for a given subreddit.
+If an invalid subreddit is given, the function returns 0.
+"""
+
 import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API to get the number of subscribers for a given subreddit.
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
     
     Args:
         subreddit (str): The name of the subreddit to query.
-    
+
     Returns:
-        int: The number of subscribers for the subreddit. Returns 0 if the subreddit
-             is invalid or any error occurs.
+        int: The number of subscribers for the given subreddit.
+             Returns 0 if the subreddit is invalid.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'my-app/0.0.1'}
     
     try:
-        # Make the GET request
         response = requests.get(url, headers=headers, allow_redirects=False)
         
-        # Check if the subreddit is valid
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
@@ -27,10 +31,3 @@ def number_of_subscribers(subreddit):
             return 0
     except Exception as e:
         return 0
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))

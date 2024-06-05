1bf8f0168b1940f0722f0f13a53e27c1403d501f
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-06-05 08:49:08 +0000

    Creted api to check no of subscriberes for each subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    # Define the URL and headers
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    
+    try:
+        # Make the GET request
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        # Check if the subreddit is valid
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        return 0
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))

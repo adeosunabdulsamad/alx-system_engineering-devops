91529a8e658de9a5a00e8de66c4a5290e9e8f712
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-06-11 07:22:40 -0700

    Check it

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
-ne { 'Turn off passwd auth':
-  path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no',
-  match  => '^#?PasswordAuthentication',
+# Turn off password authentication in SSH server configuration
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/sshd_config',        # Path to the SSH server configuration file
+  line   => 'PasswordAuthentication no',    # Line to add or modify in the file
+  match  => '^#?PasswordAuthentication',    # Regular expression to match the line
 }
 
+# Declare the private key file for SSH client
 file_line { 'Declare identity file':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?IdentityFile',
+  path   => '/etc/ssh/ssh_config',         # Path to the SSH client configuration file
+  line   => 'IdentityFile ~/.ssh/school',   # Line to add or modify in the file
+  match  => '^#?IdentityFile',              # Regular expression to match the line
 }

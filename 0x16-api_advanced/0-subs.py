546d0ecd2159855b6034de62dadc3e0106b1f0c3
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-08-13 21:00:13 +0100

    fixed it to pep 8 style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,11 +2,13 @@
 """Module to query the Reddit API and returns the number of subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     headers = {'User-Agent': 'MyRedditAPI/0.1 by YourUsername'}
-    response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers, allow_redirects=False)
-                
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200 and 'data' in response.json():
         data = response.json()
         return data.get('data', {}).get('subscribers', 0)

fdec37d62060ba6e01d32cfe852a9a36d40ba036
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-08-13 22:47:16 +0100

    feat: Add recursive function to retrieve hot posts from a subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module that prints list of posts listed for a given subreddit"""
+import requests
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Recursively queries the Reddit API and returns a list of hot articles
+    in a given subreddit. If no results are found or the s.
+    """
+    h = {'User-Agent': 'My-User-Agent'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    p = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=h, params=p, allow_redirects=False)
+
+    # Check for a valid response
+    if response.status_code != 200:
+        return None
+
+    data = response.json().get('data', {})
+    children = data.get('children', [])
+    after = data.get('after')
+
+    # Append titles to the hot_lis
+    for post in children:
+        hot_list.append(post.get('data', {}).get('title', ''))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
+
+# Example usage:
+# print(recurse('programming'))  # Will return a list of all hot article titles in the 'programming' subreddit
+# print(recurse('this_is_a_fake_subreddit'))  # Will return None

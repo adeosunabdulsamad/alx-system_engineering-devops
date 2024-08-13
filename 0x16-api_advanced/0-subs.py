ccd9f48bed834e4fe69741578eec626062bc9c09
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-08-13 09:14:03 -0700

    used data parsing to get tye infp from api endpoint

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+import requests
 
 def number_of_subscribers(subreddit):
-    {
-            }
+    headers = {'User-Agent': 'MyRedditAPI/0.1 by YourUsername'}
+    response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers, allow_redirects=False)
+                
+    if response.status_code == 200 and 'data' in response.json():
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+    else:
+        return 0

7fc6b3b3b879a8a00f6528ddbd97becc97c3748c
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2024-08-13 20:23:58 +0100

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module to query the Reddit API and returns the number of subscribers"""
 import requests
 
 def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
     headers = {'User-Agent': 'MyRedditAPI/0.1 by YourUsername'}
     response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers, allow_redirects=False)
                 
